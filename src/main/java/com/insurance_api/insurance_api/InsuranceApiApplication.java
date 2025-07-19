98463aacbffddf139839c94aa31ab524d569f395
Viktor Falkesjö <viktorfalkesjo@gmail.com>
2025-07-19 13:43:04 +0200

    first commit, setup spring boot project, postgresql

---
+++ src/main/java/com/insurance_api/insurance_api/InsuranceApiApplication.java
@@ -0,0 +1,12 @@
+package com.insurance_api.insurance_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceApiApplication.class, args);
+	}
+}
